9db25d4beceb1c8b7a866015980ac7c285886146
Noa <noafalik13@gmail.com>
2023-05-01 23:15:10 +0300

    appRouters and welcome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AppRouters from './appRouters';
 
 function App() {
   return (
     <div className="App">
-        
+        <AppRouters/>
     </div>
   );
 }
