8164ccebd8b5ae6197108286ec8d2f9aa347c759
Noa <noafalik13@gmail.com>
2023-05-15 13:42:15 +0300

    server delete

--- server/routes/contenders.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", async(req,res) => {
-  res.json({msg:"contenders work 15:11"});
-})
-
-module.exports = router;
